41abebca5e68c0a23110de2298d67e73b32e1abe
takahashim <maki@rubycolor.org>
2012-11-22 16:46:43 +0900

    initial commit

---
+++ lib/redcarpet-review.rb
@@ -0,0 +1,7 @@
+require "redcarpet-review/version"
+
+module Redcarpet
+  module Review
+    # Your code goes here...
+  end
+end
---
+++ lib/redcarpet-review/version.rb
@@ -0,0 +1,5 @@
+module Redcarpet
+  module Review
+    VERSION = "0.0.1"
+  end
+end
