8869ec6bfaf7ed90c7cfa517d7ce8c6df31c0377
takahashim <maki@rubycolor.org>
2012-11-23 19:07:24 +0900

    add command md2review and ReVIEW renderer

---
+++ lib/redcarpet/render/review.rb
@@ -0,0 +1,135 @@
+module Redcarpet
+  module Render
+    class ReVIEW < Base
+
+      def initialize(render_extensions)
+        super()
+        @table_num = 0
+        @table_id_prefix = "tbl"
+      end
+
+      def normal_text(text)
+        text
+      end
+
+      def block_code(code, language)
+        code_text = normal_text(code).chomp
+        lang = ""
+        if language
+          lang = "#\@# lang: #{language}\n"
+        end
+        "\n#{lang}//emlist{\n#{code_text}\n//}\n"
+      end
+
+      def block_quote(quote)
+        quote_text = normal_text(quote).chomp
+        quote_text.gsub!(/\A\n\n/,'')
+        "\n//quote{\n#{quote_text}\n//}\n"
+      end
+
+      def block_html(raw_html)
+        html_text = raw_html.chomp
+        warning = "XXX: BLOCK_HTML: YOU SHOULD REWRITE IT"
+        "\n//emlist{\n#{warning}\n#{html_text}\n//}\n"
+      end
+
+      def hrule
+        "\n//hr\n"
+      end
+
+      def codespan(code)
+        "@<tt>{#{normal_text(code)}}"
+      end
+
+      def header(title, level)
+        case level
+        when 1
+          "\n= #{title}\n"
+        when 2
+          "\n== #{title}\n"
+        when 3
+          "\n=== #{title}\n"
+        when 4
+          "\n==== #{title}\n"
+        end
+      end
+
+      def table(header, body)
+        @sep = nil
+        header_text = ""
+        if header
+          header_text = "#{header}-----------------\n"
+        end
+        "//table[#{table_id()}][]{\n#{header_text}#{body}\n//}\n"
+      end
+
+      def table_row(content)
+        @sep = nil
+        content+"\n"
+      end
+
+      def table_cell(content, alignment)
+        sep = @sep
+        @sep = "\t"
+        "#{sep}#{content}"
+      end
+
+      def image(link, title, alt_text)
+        filename = File.basename(link,".*")
+        "//image[#{filename}][#{alt_text}]{\n//}"
+      end
+
+      def autolink(link, link_type)
+        "@<href>{#{link}}"
+      end
+
+      def link(link, title, content)
+        "@<href>{#{link},#{content}}"
+      end
+
+      def double_emphasis(text)
+        "@<strong>{#{text}}"
+      end
+
+      def emphasis(text)
+        "@<b>{#{text}}"
+      end
+
+      def strikethrough(text)
+        "@<del>{#{text}}"
+      end
+
+      def linebreak
+        "@<br>{}\n"
+      end
+
+      def paragraph(text)
+        "\n\n#{text}\n"
+      end
+
+      def list(content, list_type)
+        case list_type
+        when :ordered
+          "\n#{content}\n"
+        when :unordered
+          "\n#{content}\n"
+        end
+      end
+
+      def list_item(content, list_type)
+        case list_type
+        when :ordered
+          " 1. #{content.strip}\n"
+        when :unordered
+          " * #{content.strip}\n"
+        end
+      end
+
+      def table_id()
+        @table_num += 1
+        "#{@table_id_prefix}#{@table_num}"
+      end
+
+    end
+  end
+end
